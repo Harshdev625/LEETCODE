047a327ffd90592bec45bb91278eb7c464bbe169
HARSH DEV <98793412+Harshdev625@users.noreply.github.com>
2023-01-25 15:10:19 +0530

    Create Finding_the_Users_Active_Minutes.cpp
    Create Finding_the_Users_Active_Minutes.cpp

---
+++ 1817. Finding the Users Active Minutes/Finding_the_Users_Active_Minutes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        vector<int>ans(k,0);
+        unordered_map<int,set<int>>m;
+        for(int i=0;i<logs.size();i++){
+            m[logs[i][0]].insert(logs[i][1]);
+        }
+        for(auto &i:m){
+            if(i.second.size()!=0)
+            ans[i.second.size()-1]++;
+        }
+        return ans;
+    }
+};
