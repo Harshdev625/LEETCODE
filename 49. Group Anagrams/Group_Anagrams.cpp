bee466c686e7010f1c87fc0887a4cb460030a4f0
HARSH DEV <98793412+Harshdev625@users.noreply.github.com>
2023-02-04 10:55:37 +0530

    Create Group_Anagrams.cpp

---
+++ 49. Group Anagrams/Group_Anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<string, vector<string>> m;
+        for (string str : strs) {
+            string t = str;
+            sort(t.begin(), t.end());
+            m[t].push_back(str);
+        }
+        for (auto i : m) {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
