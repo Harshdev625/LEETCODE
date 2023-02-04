09af19d91ee15783f0ffd5037bf2f09046de3970
HARSH DEV <98793412+Harshdev625@users.noreply.github.com>
2023-02-04 10:56:59 +0530

    Update Group_Anagrams.cpp

--- 49. Group Anagrams/Group_Anagrams.cpp
+++ 49. Group Anagrams/Group_Anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        map<string, vector<string>> m;
+        unordered_map<string, vector<string>> m;
         for (string str : strs) {
             string t = str;
             sort(t.begin(), t.end());
