49c9ea902cdbfdda64a5d1ff3b57b6b6fb710591
HARSH DEV <98793412+Harshdev625@users.noreply.github.com>
2023-02-20 09:22:56 +0530

    Create Search_Insert_Position.cpp

---
+++ 35. Search Insert Position/Search_Insert_Position.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1, mid;
+        while (start <= end) {
+            mid = (start + end) >>1;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid - 1;
+            else start = mid + 1;
+        }
+        return start;
+    }
+};
