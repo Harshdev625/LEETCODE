6b0d3a7336b4ee195ef0c91c058c8cbb82983e7a
HARSH DEV <98793412+Harshdev625@users.noreply.github.com>
2023-03-20 09:32:42 +0530

    Update Design_Browser_History.cpp

--- 1472. Design Browser History/Design_Browser_History.cpp
+++ 1472. Design Browser History/Design_Browser_History.cpp
@@ -1,3 +1,4 @@
+// Approach 1: Using Inbuilt List
 class BrowserHistory {
     list<string>history;
     list<string> :: iterator it;
@@ -33,3 +34,52 @@ public:
  * string param_2 = obj->back(steps);
  * string param_3 = obj->forward(steps);
  */
+
+// Approach 2: By making a Node class
+class Node{
+    public:
+    string s;
+    Node*next;
+    Node*prev;
+    Node(string s){
+        this->s=s;
+        next=NULL;
+        prev=NULL;
+    }
+};
+class BrowserHistory {
+public:
+    Node*traverse;
+    BrowserHistory(string homepage) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        Node*head=new Node(homepage);
+        traverse=head;
+    }
+    
+    void visit(string url) {
+        Node*temp=new Node(url);
+        traverse->next=temp;
+        temp->prev=traverse;
+        traverse=temp;
+    }
+    
+    string back(int steps) {
+        while(steps-- && traverse->prev!=NULL)traverse=traverse->prev;
+        return traverse->s;
+    }
+    
+    string forward(int steps) {
+        while(steps-- && traverse->next!=NULL)traverse=traverse->next;
+        return traverse->s;
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */ 
